bbefcddb2cbdb62f9da6d9efb90d68832d2e8940
Carol Tam <cover123456@hotmail.com>
2023-08-26 23:19:37 +0800

    First Post

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./src/css/style.css');
+    // Return your Object options:
+    return {
+      dir: {
+        input: "src", // default is "."
+        output: "_site" // default is "_site"
+      }
+    }
+  };

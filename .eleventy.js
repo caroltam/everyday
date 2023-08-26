ad3b218971118fba4a5cc41bb76f35245f73cabc
Carol Tam <cover123456@hotmail.com>
2023-08-26 23:47:06 +0800

    Fix the relative path

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/style.css');
     // Return your Object options:
     return {
-      pathPrefix: "everyday",
+      pathPrefix: "/everyday/",
       dir: {
         input: "src", // default is "."
         output: "_site" // default is "_site"
